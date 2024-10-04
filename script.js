4775c58735a57604ec5e1eefa66c8265490558a2
Wiggle-Ryutsuki <maimoonaaz15@gmail.com>
2024-10-05 00:49:40 +0700

    Editting scroll settings

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
         });
     });
 });
