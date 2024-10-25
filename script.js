c3f8c139bef1d3b5caee99ec5a806195f2b3e958
Wiggle-Ryutsuki <maimoonaaz15@gmail.com>
2024-10-25 12:16:15 +0000

    Oct 25, 2024, 7:16 PM

--- script.js
+++ script.js
@@ -22,5 +22,27 @@ document.addEventListener('DOMContentLoaded', () => {
             header.classList.remove('scrolled');
         }
     });
+
+    skillProgress();
+    function skillProgress(){
+        let i = 0;
+        function move() {
+          if (i == 0) {
+            i = 1;
+            let elem = document.getElementById("myBar");
+            let width = 1;
+            let id = setInterval(frame, 10);
+            function frame() {
+              if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+              } else {
+                width++;
+                elem.style.width = width + "%";
+              }
+            }
+          }
+        }
+    };
     
 });
